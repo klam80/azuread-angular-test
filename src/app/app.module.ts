f045bec5124501a8c8141509267aa35324efdc16
klam80 <56801780+klam80@users.noreply.github.com>
2022-04-06 19:03:51 -0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,79 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AzureAdDemoService } from './azure-ad-demo.service';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReportComponent } from './report/report.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+const isIE=window.navigator.userAgent.indexOf('MSIE')>-1
+||window.navigator.userAgent.indexOf('Trident/')>-1
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ReportComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatDividerModule,
+    HttpClientModule,
+    MsalModule.forRoot(new PublicClientApplication
+      (
+        {
+          auth:{
+            clientId:'1421bb55-09d2-4dee-8f4b-b8b4ddccf25e',
+            redirectUri:'http://localhost:4200',
+            authority:'https://login.microsoftonline.com/32ab84c9-e6de-4459-a85c-9156dfb35149'
+          },
+          cache:
+          {
+            cacheLocation:'localStorage',
+            storeAuthStateInCookie:isIE
+          }
+        }
+      ),
+      {
+        interactionType:InteractionType.Redirect,
+        authRequest:{
+          scopes:['user.read']
+        }
+      },
+      {
+        interactionType:InteractionType.Redirect,
+        protectedResourceMap:new Map(
+          [
+            ['https://graph.microsoft.com/v1.0/me',['user.Read']], // [an array of permissions. by default user.Read]
+            ['localhost',['api://1366afa1-5b81-490f-b61d-1c1100008ea7/api.scope']] // copied from azure "Expose an API". localhost because that is where the web api runs
+          ]
+        )
+      }
+      )
+  ],
+  providers: [{
+    provide:HTTP_INTERCEPTORS,
+    useClass:MsalInterceptor, // will add the token to each http req header
+    multi:true
+  },MsalGuard,AzureAdDemoService],
+  bootstrap: [AppComponent,MsalRedirectComponent]
+})
+export class AppModule { }
